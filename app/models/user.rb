a87522561702e28d98c3d25582365e9388c5fe15
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-22 18:29:33 +0000

    create new authentication method to check the email existence and the password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ActiveRecord::Base
 
   # This validation serves for the tests. Without validation, a nil value would mean that the confirmation is not needed and thus a user would be created
   validates :password_confirmation, presence: true, length: { minimum: 5 }
+
+  # Check if the user exists, and check if the password if correct. Return the user instance or nil.
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email).try(:authenticate, password)
+    if @user
+      @user
+    else
+      nil
+    end
+  end
+
 end
