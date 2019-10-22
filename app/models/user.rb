c0f583ba87e21e2aa10e1079cc139f3dcebbe7e8
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-22 03:29:07 +0000

    check if the email already exists in the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_presence_of :first_name, :last_name, :email
+  validates_uniqueness_of :email, :case_sensitive => false
 
   # This validation serves for the tests. Without validation, a nil value would mean that the confirmation is not needed and thus a user would be created
   validates :password_confirmation, presence: true
