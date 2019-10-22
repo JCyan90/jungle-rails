983d5fb9a25275601c7cbc1fc9e405bc0d0304c8
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-22 03:39:47 +0000

    check the length of the password is > 5 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email, :case_sensitive => false
 
   # This validation serves for the tests. Without validation, a nil value would mean that the confirmation is not needed and thus a user would be created
-  validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true, length: { minimum: 5 }
 end
